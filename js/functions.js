e04cff558a5d728dd89f432462cffd2601b276b8
NoraLili <noraliliwork@gmail.com>
2023-09-07 14:20:32 +0200

    11-15 exercises

--- js/functions.js
+++ js/functions.js
@@ -243,6 +243,251 @@ function printTravelCost() {
     let gasolinePrice = Number(prompt(`Please enter the price of gasoline (euro per liter)!`));
     let lengthOfJurney = Number(prompt(`Please enter the length of the journey (km)!`));
 
+    if (!validateNumber(consumption) || !validateNumber(gasolinePrice) || !validateNumber(lengthOfJurney)) {
+        alert(`You enterred an incorrect data! Please enter a number!`);
+        return;
+    }
     let result = `The consumption of the car is: ${consumption} liter/100 km, the price of gasoline is: ${gasolinePrice} €/liter and the length of the journey is: ${lengthOfJurney} km. <br>The travel cost is: ${calculateTravelCost(consumption, gasolinePrice, lengthOfJurney)} €.`;
     document.getElementById(`solution-10`).innerHTML = result;
+}
+
+// Exercise 11
+
+function exchangeEuro(euroExchangeRate, euroAmmount) {
+    return euroExchangeRate * euroAmmount;
+}
+
+function printExchange() {
+    let euroExchangeRate = Number(prompt(`Please enter the euro exchange rate (how much is 1 € worth in HUF)!`));
+    let euroAmmount = Number(prompt(`Please enter the amount of euros you want to convert to HUF!`));
+
+    if (!validateNumber(euroExchangeRate) || euroExchangeRate < 1) {
+        alert(`You enterred an incorrect data! Please enter a positive number!`);
+        return;
+    }
+    if (!validateIntegerNumber(euroAmmount) || euroAmmount < 1) {
+        alert(`You enterred an incorrect data! Please enter a positive integer number!`);
+        return;
+    }
+    let result = `1 € = ${euroExchangeRate} HUF <br>Your desired amount is ${euroAmmount} €, which is ${exchangeEuro(euroExchangeRate, euroAmmount)} HUF.`;
+    document.getElementById(`solution-11`).innerHTML = result;
+}
+
+// Exercise 12
+function calculateCubeSurface(cubeSide) {
+    return 6 * (Math.pow(cubeSide, 2));
+}
+
+function calculateCubeVolume(cubeSide) {
+    return Math.pow(cubeSide, 3);
+}
+
+function printCubeSurfaceAndVolume() {
+    let cubeSide = Number(prompt(`Please enter a number (side of the cube)!`));
+
+    if (!validateNumber(cubeSide) || cubeSide < 1) {
+        alert(`You enterred an incorrect data! Please enter a positive number!`);
+        return;
+    }
+    let result = `The side of the cube is: ${cubeSide}, therefore the surface is: ${calculateCubeSurface(cubeSide)} and volume is: ${calculateCubeVolume(cubeSide)}.`;
+    document.getElementById(`solution-12`).innerHTML = result;
+}
+
+// Exercise 13
+function calculateConeSurface(coneRadius, coneHeight) {
+    let surface = Math.PI * coneRadius * (coneRadius + ((Math.sqrt(Math.pow(coneRadius, 2) + Math.pow(coneHeight, 2)))));
+    return surface.toFixed(3);
+}
+
+function calculateConeVolume(coneRadius, coneHeight) {
+    let volume = (1 / 3) * (Math.PI * Math.pow(coneRadius, 2) * coneHeight);
+    return volume.toFixed(3);
+}
+
+function printConeSurfaceAndVolume() {
+    let coneRadius = Number(prompt(`Please enter a number (radius of the cone)!`));
+    let coneHeight = Number(prompt(`Please enter a number (height of the cone)!`));
+
+    if (!validateNumber(coneRadius) || coneRadius < 1 || !validateNumber(coneHeight) || coneHeight < 1) {
+        alert(`You enterred an incorrect data! Please enter a positive number!`);
+        return;
+    }
+    let result = `The radius of the cone is: ${coneRadius}, the height is: ${coneHeight}. <br>Therefore the surface is ${calculateConeSurface(coneRadius, coneHeight)} and volume is ${calculateConeVolume(coneRadius, coneHeight)}.`;
+    document.getElementById(`solution-13`).innerHTML = result;
+}
+
+// Exercise 14/a
+function createArray(lenghtOfArray, lowerLimit, upperLimit) {
+    if (lowerLimit > upperLimit) {
+        alert(`The upper limit has to be greather than the lower limit!`);
+        return;
+    }
+    let randomArray = [];
+    for (let i = 0; i < lenghtOfArray; i++) {
+        let element = Math.floor(Math.random() * (upperLimit - lowerLimit + 1)) + lowerLimit;
+        randomArray.push(element)
+    }
+    return randomArray;
+}
+
+function printArrayResults() {
+    let lenghtOfArray = Math.floor(Math.random() * (20 - 5 + 1)) + 5;
+    // console.log(lenghtOfArray);
+    let lowerLimit = Number(prompt(`Please enter the lower limit of random numbers!`));
+    let upperLimit = Number(prompt(`Please enter the upper limit of random numbers!`));
+
+    if (!validateIntegerNumber(lenghtOfArray) || lenghtOfArray < 1) {
+        alert(`You enterred an incorrect data! Please enter a positive integer number!`);
+        return;
+    }
+    if (!validateIntegerNumber(lowerLimit) || !validateIntegerNumber(upperLimit)) {
+        alert(`You enterred an incorrect data! Please enter an integer number!`);
+        return;
+    }
+
+    let randomArray = createArray(lenghtOfArray, lowerLimit, upperLimit);
+
+    let result = `Your array is: ${randomArray.join(`, `)}`;
+    document.getElementById(`solution-14-a`).innerHTML = result;
+
+    document.getElementById(`solution-14-b`).innerHTML = calculateSumOfEvens(randomArray);
+
+    document.getElementById(`solution-14-c`).innerHTML = calculateAverageOfOdds(randomArray);
+
+    document.getElementById(`solution-14-d`).innerHTML = `The smallest value is: ${determineSmallestValue(randomArray)}`;
+
+    document.getElementById(`solution-14-e`).innerHTML = `The index/indices of the smallest elements is/are: ${determineIndexOfSmallestValue(randomArray)}`;
+
+    document.getElementById(`solution-14-f`).innerHTML = `One random element from the array: ${searchRandomElement(randomArray)}`;
+
+    document.getElementById(`solution-14-g`).innerHTML = `Every 5th elements from the array: ${searchFifthElements(randomArray)}`;
+
+    document.getElementById(`solution-14-h`).innerHTML = searchDivisibleByThree(randomArray);
+}
+
+// Exercise 14/b
+function calculateSumOfEvens(array) {
+    let sumEvens = 0;
+    array.forEach(element => {
+        if (element % 2 == 0) {
+            sumEvens += element;
+        }
+    });
+    if (sumEvens) {
+        return `The sum of the even elements is: ${sumEvens}`;
+    } else {
+        return `There are no even elements in the array!`;
+    }
+}
+
+// Exercise 14/c
+function calculateAverageOfOdds(array) {
+    let sumOdds = 0;
+    let counter = 0;
+    array.forEach(element => {
+        if (element % 2 !== 0) {
+            sumOdds += element;
+            counter++;
+        }
+    });
+    if (counter !== 0) {
+        return `The average of the odd elements is: ${(sumOdds / counter).toFixed(1)}`;
+    } else {
+        return `There are no odd elements in the array!`;
+    }
+}
+
+// Exercise 14/d
+function determineSmallestValue(array) {
+    let smallestElement = array[0];
+    array.forEach(element => {
+        if (element < smallestElement) {
+            smallestElement = element;
+        }
+    });
+    return smallestElement;
+}
+
+// Exercise 14/e
+function determineIndexOfSmallestValue(array) {
+    let indices = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === determineSmallestValue(array)) {
+            indices.push(i);
+        }
+    };
+    return indices.join(`, `);
+}
+
+// Exercise 14/f
+function searchRandomElement(array) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+// Exercise 14/g
+function searchFifthElements(array) {
+    let fifthElements = [];
+    for (let i = 4; i < array.length; i += 5) {
+        fifthElements.push(array[i]);
+    };
+    return fifthElements.join(`, `);
+}
+
+// Exercise 14/h
+function searchDivisibleByThree(array) {
+    let divisibleByThree = [];
+    let counter = 0;
+    array.forEach(element => {
+        if (element % 3 == 0) {
+            divisibleByThree.push(element);
+            counter++;
+        }
+    });
+    if (counter > 0) {
+        return `These numbers are divisible by three without
+            a remainder: ${divisibleByThree.join(`, `)}`;
+    } else {
+        return `There are no numbers in the array that are divisible by three without
+            a remainder!`;
+    }
+}
+
+// Exercise 15
+function calculateBmi(usersWeight, usersHeight) {
+    let usersHeightInMeter = usersHeight / 100;
+    return (usersWeight / (Math.pow(usersHeightInMeter, 2))).toFixed(2);
+}
+
+function changeBmiToText(bmi) {
+    if (bmi < 16) {
+        return `seriously underweighted`;
+    } else if (16 <= bmi && bmi < 17) {
+        return `moderately underweighted`;
+    } else if (17 <= bmi && bmi < 18.5) {
+        return `mildly underweighted`;
+    } else if (18.5 <= bmi && bmi < 25) {
+        return `in the normal weight zone`;
+    } else if (25 <= bmi && bmi < 30) {
+        return `overweighted`;
+    } else if (30 <= bmi && bmi < 35) {
+        return `in Obese Class I`;
+    } else if (35 <= bmi && bmi < 40) {
+        return `in Obese Class II`;
+    } else if (40 <= bmi) {
+        return `in Obese Class III (morbidly obese)`;
+    }
+}
+
+function printBmi() {
+    let usersWeight = Number(prompt(`Please enter your weight (in kilograms)!`));
+    let usersHeight = Number(prompt(`Please enter your height (in centimeters)!`));
+
+    if (!validateNumber(usersWeight) || usersWeight < 1 || !validateNumber(usersHeight) || usersHeight < 1) {
+        alert(`You enterred an incorrect data! Please enter a positive number!`);
+        return;
+    }
+    let bmi = calculateBmi(usersWeight, usersHeight);
+    let result = `Your Body Mass Index (BMI) is: ${bmi} , you are ${changeBmiToText(bmi)}.`;
+    document.getElementById(`solution-15`).innerHTML = result;
 }
